dbe56324cf04459ef7a922f5f77c4e34e54afdb6
Evgeny <sev.20@uni-dubna.ru>
2022-11-08 11:30:12 +0300

    [07] Implement mode with semaphores

--- 07_semaphores/1.cpp
+++ 07_semaphores/1.cpp
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// #include <fcntl.h>
-// #include <stdlib.h>
+#include <sys/sem.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include <sys/ipc.h>
 #include <sys/shm.h>
+
+int g_shm_fd = -1;
+int g_sem_fd = -1;
+
+void free_shared_resources() {
+    if(g_shm_fd != -1) {
+        shmctl(g_shm_fd, IPC_RMID, 0);
+    }
+    if(g_sem_fd != -1) {
+        semctl(g_sem_fd, 0, IPC_RMID);
+    }
+}
+
+void catch_sigint(int code) {
+    free_shared_resources();
+    exit(code);
+}
 
 void print_usage(char* argv0) {
     printf("Usage:\n");
@@ -29,16 +45,7 @@ void print_arr(int* arr, int N) {
     printf("\n");
 }
 
-int g_shm_fd = -1;
-
-void catch_sigint(int code) {
-    if(g_shm_fd != -1) {
-        shmctl(g_shm_fd, IPC_RMID, 0);
-    }
-    exit(code);
-}
-
-void mode_w(int N) {
+void* my_shm_init(int N) {
     g_shm_fd = shmget(IPC_PRIVATE, N * sizeof(int) + 1, IPC_CREAT | 0644);
     if(g_shm_fd < 0) {
         printf("shmget() error on create\n");
@@ -53,6 +60,20 @@ void mode_w(int N) {
     *cur_id = 0;
     init_arr(arr, -1, N);
     print_arr(arr, N);
+
+    return addr;
+}
+
+void wait_for_childs() {
+    for(int i = 0; i < 2; i++) {
+        wait(0);
+    }
+}
+
+void mode_w(int N) {
+    void* addr = my_shm_init(N);
+    int* cur_id = (int*)addr;
+    int* arr = (int*)addr + 1;
 
     // first child
     if(!fork()) {
@@ -85,20 +106,75 @@ void mode_w(int N) {
         exit(0);
     }
 
-    // wait for childs
-    for(int i = 0; i < 2; i++) {
-        wait(0);
-    }
+    wait_for_childs();
 
     print_arr(arr, N);
 
     shmdt(addr);
-    shmctl(g_shm_fd, IPC_RMID, 0);
+    free_shared_resources();
+}
+
+void sem_up(int semno) {
+    struct sembuf sb;
+    sb.sem_num = semno;
+    sb.sem_op = 1;
+    semop(g_sem_fd, &sb, 1);
+}
+void sem_down(int semno) {
+    struct sembuf sb;
+    sb.sem_num = semno;
+    sb.sem_op = -1;
+    semop(g_sem_fd, &sb, 1);
 }
 
 void mode_s(int N) {
-    // TODO:
-    printf("TODO: not yet implemented\n");
+    void* addr = my_shm_init(N);
+    int* cur_id = (int*)addr;
+    int* arr = (int*)addr + 1;
+
+    g_sem_fd = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
+    sem_up(0);
+
+    // first child
+    if(!fork()) {
+        int p1_count = 0;
+        while(*cur_id < N) {
+            sem_down(0);
+
+            p1_count++;
+            arr[*cur_id] = *cur_id;
+            (*cur_id)++;
+
+            sem_up(1);
+        }
+
+        printf("p1 count: %i\n", p1_count);
+        exit(0);
+    }
+
+    // second child
+    if(!fork()) {
+        int p2_count = 0;
+        while(*cur_id < N) {
+            sem_down(1);
+
+            p2_count++;
+            arr[*cur_id] = *cur_id;
+            (*cur_id)++;
+
+            sem_up(0);
+        }
+
+        printf("p2 count: %i\n", p2_count);
+        exit(0);
+    }
+
+    wait_for_childs();
+
+    print_arr(arr, N);
+
+    shmdt(addr);
+    free_shared_resources();
 }
 
 int main(int argc, char* argv[]) {
